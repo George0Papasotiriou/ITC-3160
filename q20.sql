dc63f5538a491d6399df84363ff3ace43b96040e
George Papasotiriou <123271840+George0Papasotiriou@users.noreply.github.com>
2023-06-25 14:48:38 +0300

    Update q20.sql

--- q20.sql
+++ q20.sql
@@ -1,22 +1,47 @@
 --20
 CREATE OR REPLACE TRIGGER update_numEmployeesAssigned
-AFTER INSERT OR UPDATE OR DELETE ON Assign
-FOR EACH STATEMENT
-BEGIN
-    -- Update numEmployeesAssigned for new assignments
-    UPDATE Project p
-    SET numEmployeesAssigned = numEmployeesAssigned + 
-        (SELECT COUNT(*) FROM Assign a WHERE a.projNo = p.projNo)
-    WHERE EXISTS (
-        SELECT 1 FROM Assign a WHERE a.projNo = p.projNo
-    );
+FOR INSERT OR UPDATE OR DELETE ON Assign
+COMPOUND TRIGGER
 
-    -- Update numEmployeesAssigned for deleted assignments
-    UPDATE Project p
-    SET numEmployeesAssigned = numEmployeesAssigned - 
-        (SELECT COUNT(*) FROM Assign a WHERE a.projNo = p.projNo)
-    WHERE EXISTS (
-        SELECT 1 FROM Assign a WHERE a.projNo = p.projNo
-    );
-END;
+  -- Define a collection to store project numbers
+  TYPE projNoList IS TABLE OF Assign.projNo%TYPE;
+  projNos projNoList := projNoList();
+
+  AFTER EACH ROW IS BEGIN
+    -- Store the project number of the affected row
+    projNos.EXTEND();
+    projNos(projNos.COUNT) := :new.projNo;
+  END AFTER EACH ROW;
+
+  AFTER STATEMENT IS BEGIN
+    -- Iterate through the project numbers and update the numEmployeesAssigned field
+    FOR i IN 1..projNos.COUNT LOOP
+      -- Calculate the number of employees
+      DECLARE
+        totalEmployees NUMBER;
+      BEGIN
+        SELECT COUNT(*) INTO totalEmployees
+        FROM Assign
+        WHERE projNo = projNos(i);
+
+        -- Update numEmployeesAssigned field
+        UPDATE Project
+        SET numEmployeesAssigned = totalEmployees
+        WHERE projNo = projNos(i);
+      END;
+    END LOOP;
+    
+    -- Clear the project number list
+    projNos.DELETE;
+  END AFTER STATEMENT;
+
+END update_numEmployeesAssigned;
 /
+
+
+ALTER TABLE Project ADD numEmployeesAssigned NUMBER;
+
+--For Testing Purposes
+INSERT INTO ASSIGN Values(1057, 200, 5, 5);
+
+SELECT * FROM ASSIGN;
